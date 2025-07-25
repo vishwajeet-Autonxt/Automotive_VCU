a920fcfc19be07d3ed76b7e596a33c6f472894ed
vishwajeet-Autonxt <vishwajeet@autonxt.in>
2025-07-25 17:16:52 +0530

    Added BatteryDischarge.h & .c file

---
+++ Features/BatteryDischarge.c
@@ -0,0 +1,8 @@
+/*
+ * BatteryDischarge.c
+ *
+ *  Created on: 25-Jul-2025
+ *      Author: Vishwajeet_Jagtap
+ */
+
+
---
+++ Features/BatteryDischarge.h
@@ -0,0 +1,13 @@
+/*
+ * BatteryDischarge.h
+ *
+ *  Created on: 25-Jul-2025
+ *      Author: Vishwajeet_Jagtap
+ */
+
+#ifndef BATTERYDISCHARGE_H_
+#define BATTERYDISCHARGE_H_
+
+
+
+#endif /* BATTERYDISCHARGE_H_ */
