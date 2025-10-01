d8939b027c8d87a30a8da943f2b60590e230411a
vishwajeet-Autonxt <vishwajeet@autonxt.in>
2025-10-01 14:50:08 +0530

    Added LightCtrl.h & LightCtrl.c in BCM folder

---
+++ BCM/LightCtrl.c
@@ -0,0 +1,8 @@
+/*
+ * LightCtrl.c
+ *
+ *  Created on: 01-Oct-2025
+ *      Author: Vishwajeet_Jagtap
+ */
+
+
---
+++ BCM/LightCtrl.h
@@ -0,0 +1,13 @@
+/*
+ * LightCtrl.h
+ *
+ *  Created on: 01-Oct-2025
+ *      Author: Vishwajeet_Jagtap
+ */
+
+#ifndef LIGHTCTRL_H_
+#define LIGHTCTRL_H_
+
+
+
+#endif /* LIGHTCTRL_H_ */
