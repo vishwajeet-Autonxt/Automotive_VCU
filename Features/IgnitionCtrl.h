6b01093277f784071fac75106f83fc928b992776
vishwajeet-Autonxt <vishwajeet@autonxt.in>
2025-10-09 17:41:15 +0530

    Added ignition control file

--- Features/IgnitionCtrl.h
+++ Features/IgnitionCtrl.h
@@ -10,11 +10,25 @@
 
 #include <stdbool.h>
 
-void Ignition_Init(void);
+//void Ignition_Init(void);
 
 // Stage 2 (Battery Discharge) control
-void Ignition_EnableStage2(void);
-void Ignition_DisableStage2(void);
-bool Ignition_IsStage2Enabled(void);
+//void Ignition_EnableStage2(void);
+//void Ignition_DisableStage2(void);
+//bool Ignition_IsStage2Enabled(void);
+
+#include "S32K144.h"
+
+// GPIO pin definitions
+#define IGN_ON_PIN       10   // PTC10: ignition ON stage
+#define START_MOM_PIN    11   // PTB11: momentary START detection
+#define BLUE_LED_PIN      0   // PTD0: ON stage LED
+#define RED_LED_PIN      15   // PTD15: motor/momentary LED
+
+// Function prototypes
+void Ignition_Init(void);
+void Ignition_Task(void);
 
 #endif /* IGNITIONCTRL_H_ */
+
+
